8acdfceccd8814cd5f1b248647e92eec19a3124b
Doug Swisher <big.swish@gmail.com>
2015-03-04 19:41:27 -0600

    Initial profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+# echo Hi
+
+# Load Jump-Location profile
+Import-Module 'D:\ProgramData\Chocolatey\lib\Jump-Location.0.6.0\tools\Jump.Location.psd1'
+
+
+# Load posh-git example profile
+. 'D:\Users\Doug\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
