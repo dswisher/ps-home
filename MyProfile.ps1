d604dae8154b36257d9d3093d9d0b127798d6dfb
Doug Swisher <big.swish@gmail.com>
2015-03-04 20:06:49 -0600

    First cut at bootstrapper

---
+++ MyProfile.ps1
@@ -0,0 +1,9 @@
+
+echo "Hello from MyProfile!"
+
+# Load Jump-Location profile
+Import-Module 'D:\ProgramData\Chocolatey\lib\Jump-Location.0.6.0\tools\Jump.Location.psd1'
+
+
+# Load posh-git example profile
+. 'D:\Users\Doug\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
